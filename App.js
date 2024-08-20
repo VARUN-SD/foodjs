ccdf5ce23bcfd419e7412cb41967a8f589f0dfbe
VARUN-SD <varundara98@gmail.com>
2024-08-21 01:06:59 +0530

    start

---
+++ App.js
@@ -0,0 +1,10 @@
+const Applayout=()=>{
+        return (
+            <div>
+                <h1>Applayout Page</h1>
+            </div>
+        )
+    };
+
+const root=ReactDOM.createRoot(document.getElementById("main"));
+root.render(<Applayout/>);
